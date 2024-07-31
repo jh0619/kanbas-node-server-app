71c758b2b06d35807729a8cde5267bcdf31a4f0c
Jia Hou <jiahou961@gmail.com>
2024-07-30 21:15:10 -0700

    completes a6

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./User/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import session from "express-session";
+
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-suf";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 Hello(app);
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
